4e9991ae505a97d4d471d1f34339b37237d0e715
Isaiah cannings <89054938+blhcode@users.noreply.github.com>
2025-01-30 21:38:27 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-})
 radio.setGroup(1)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(16)
 basic.forever(function () {
-	
-})
-basic.forever(function () {
-    basic.showNumber(pins.digitalReadPin(DigitalPin.P0))
     radio.sendNumber(pins.digitalReadPin(DigitalPin.P0))
 })
